2787e86e02efe0db72d1b058cb14bb2774123914
AndrewAscone <andrew.ascone@gmail.com>
2022-10-27 13:26:06 -0400

    Further edited the test function to have the expected outputs print when they run

--- src/test/java/LogginLabTest.java
+++ src/test/java/LogginLabTest.java
@@ -39,13 +39,13 @@ public class LogginLabTest {
         LogginLab lab = new LogginLab();
         lab.setThreshold(finalLimit);
 
-        for (Integer i = 1; i <= finalLimit; i++) {
-            if (lab.thresholdReached(i)) {
+        for (Integer i = 1; i <= finalLimit + 1; i++) {
+            if (!lab.thresholdReached(i)) {
                 logger.log(Level.INFO, "Threshold not reached! It is " + i);
-                assertTrue(lab.thresholdReached(i));
+                assertFalse(lab.thresholdReached(i));
             } else {
                 logger.log(Level.INFO, "Threshold has been reached!");
-                assertFalse(lab.thresholdReached(i));
+                assertTrue(lab.thresholdReached(i));
             }
         }
 
